e0cf10358ea16114ae1b63413c12b8cafa154565
mwlodarz <marcin.wlodarz@pentacomp.pl>
2016-10-04 14:46:32 +0200

    build fix

--- iKudo/src/iKudo.Clients.Web/Controllers/HomeController.cs
+++ iKudo/src/iKudo.Clients.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace iKudo.Clients.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View()
+            return View();
         }
 
         public IActionResult Error()
