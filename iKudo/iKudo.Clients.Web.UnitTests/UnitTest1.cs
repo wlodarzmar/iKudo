c593d55b132a25da42fb2014ad71e9f007ecf744
mwlodarz <marcin.wlodarz@pentacomp.pl>
2016-10-05 16:41:27 +0200

    test project

---
+++ iKudo/iKudo.Clients.Web.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace iKudo.Clients.Web.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
