d95964bd707532f1d1daa7167852a636f3384e94
MW <MW@MW-PC>
2016-11-25 22:20:05 +0100

    przykład pobierania danych z serwera

--- iKudo/src/iKudo.Clients.Web/src/app.ts
+++ iKudo/src/iKudo.Clients.Web/src/app.ts
@@ -8,7 +8,8 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
+      { route: 'test', name: 'test', moduleId: 'test', nav: true, title: 'Test' }
     ]);
 
     this.router = router;
