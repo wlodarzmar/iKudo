e9879b2165483af278d15f5c6568a457df1a3eba
MW <MW@MW-PC>
2017-01-19 20:20:14 +0100

    projekt testów jednostkowych dla controlerów api

---
+++ iKudo/src/iKudo.Clients.Web.Tests/CompanyControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using iKudo.Controllers.Api;
+using System;
+using Xunit;
+
+namespace iKudo.Clients.Web.Tests
+{
+    public class CompanyControllerTests
+    {
+        private readonly CompanyController companyController;
+
+        public CompanyControllerTests()
+        {
+            companyController = new CompanyController();
+        }
+
+        [Fact]
+        public void MyMethod()
+        {
+            companyController.MyMethod();
+            Assert.True(true);
+        }
+    }
+}
---
+++ iKudo/src/iKudo.Clients.Web/Controllers/Api/CompanyController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iKudo.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Company")]
+    public class CompanyController : Controller
+    {
+
+        public void MyMethod()
+        {
+            
+        }
+    }
+}
