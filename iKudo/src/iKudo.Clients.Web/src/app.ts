aeabbc01574f3841bac537b5ac782cfd688567c4
MW <MW@MW-PC>
2017-01-19 19:14:03 +0100

    usunięcie users

--- iKudo/src/iKudo.Clients.Web/src/app.ts
+++ iKudo/src/iKudo.Clients.Web/src/app.ts
@@ -70,7 +70,7 @@ export class App {
         config.title = 'iKudo';
         config.map([
             { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' },
-            { route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Github Users' },
+            //{ route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Github Users' },
             { route: 'child-router', name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
             { route: 'test', name: 'test', moduleId: 'test', nav: true, title: 'Test' }
         ]);
