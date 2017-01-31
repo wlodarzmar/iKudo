7f9580f24f2afc44510dd135680fb75a95e8d5ef
MW <MW@MW-PC>
2017-01-31 20:38:07 +0100

    prosty widok dodawania organizacji

--- iKudo/src/iKudo.Clients.Web/src/app.ts
+++ iKudo/src/iKudo.Clients.Web/src/app.ts
@@ -70,7 +70,7 @@ export class App {
         config.title = 'iKudo';
         config.map([
             { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' },
-            //{ route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Github Users' },
+            { route: 'addcompany', name: 'addcompany', moduleId: 'company/addCompany', nav: true, title: 'Add Company' },
             { route: 'child-router', name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
             { route: 'test', name: 'test', moduleId: 'test', nav: true, title: 'Test' }
         ]);
---
+++ iKudo/src/iKudo.Clients.Web/src/company/addCompany.ts
@@ -0,0 +1,48 @@
+﻿import { HttpClient, json } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
+export class AddCompany {
+
+    public name: string;
+    public description: string;
+
+    private http: HttpClient;
+
+    constructor(http: HttpClient) {
+        this.http = http;
+        http.configure(config => {
+            config.useStandardConfiguration();
+            config.withBaseUrl('http://localhost:49862/');
+            config.withDefaults({
+                credentials: 'same-origin',
+                headers: {
+                    'X-Requested-With': 'Fetch'
+                }
+            });
+        });
+        this.name = 'test name';
+        this.description = 'test desc';
+    }
+
+    submit() {
+        //let addCompanyUrl = 'api/company';
+        //let requestBody = {
+        //    method: 'post',
+        //    body: json({
+        //        creatorId: localStorage.getItem('id_token'), name: this.name, description: this.description
+        //    })
+        //};
+        //this.http.fetch(addCompanyUrl, requestAnimationFrame).then(response => console.log(response));
+
+        let getUrl = 'api/company/2';
+        this.http.fetch(getUrl, {}).then(response => response.json()).then(result => {
+            console.log(result);
+        });
+
+    }
+
+    private addCompany(name: string, creatorId: string, description: string) {
+
+    }
+}
