c234d2a53517a6f5f3b162192dbc91d57433242d
mwlodarz <marcin.wlodarz@pentacomp.pl>
2017-03-22 15:52:43 +0100

    pretty inputs added but "dirty code"

---
+++ iKudo/src/iKudo.Clients.Web/wwwroot/inputs.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+        (function () {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function () {
+                return this.replace(rtrim, '');
+            };
+        })();
+    }
+
+    [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
+        // in case the input is already filled..
+        if (inputEl.value.trim() !== '') {
+            //<!-- classie.add( inputEl.parentNode, 'input--filled' ); -->
+            inputEl.parentNode.classList.add("input--filled");
+            //<!-- element.classList.add("otherclass"); -->
+        }
+
+        // events:
+        inputEl.addEventListener('focus', onInputFocus);
+        inputEl.addEventListener('blur', onInputBlur);
+    });
+
+    function onInputFocus(ev) {
+        //<!-- classie.add( ev.target.parentNode, 'input--filled' ); -->
+        ev.target.parentNode.classList.add("input--filled");
+    }
+
+    function onInputBlur(ev) {
+        if (ev.target.value.trim() === '') {
+            //<!-- classie.remove( ev.target.parentNode, 'input--filled' ); -->
+            ev.target.parentNode.classList.remove("input--filled");
+        }
+    }
+})();
