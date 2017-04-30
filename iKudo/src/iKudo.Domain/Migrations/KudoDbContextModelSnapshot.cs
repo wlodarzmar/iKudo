19e4e20779e88a9c7d44d2ee030580438bc3737d
mwlodarz <marcin.wlodarz@pentacomp.pl>
2017-04-30 15:03:34 +0200

    implementation of addition joinRequest, timeProvider

---
+++ iKudo/src/iKudo.Domain/Migrations/KudoDbContextModelSnapshot.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using iKudo.Domain.Model;
+
+namespace iKudo.Domain.Migrations
+{
+    [DbContext(typeof(KudoDbContext))]
+    partial class KudoDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("iKudo.Domain.Model.Board", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreationDate");
+
+                    b.Property<string>("CreatorId")
+                        .IsRequired();
+
+                    b.Property<string>("Description");
+
+                    b.Property<DateTime?>("ModificationDate");
+
+                    b.Property<string>("Name")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Boards");
+                });
+
+            modelBuilder.Entity("iKudo.Domain.Model.JoinRequest", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BoardId");
+
+                    b.Property<string>("CandidateId");
+
+                    b.Property<DateTime>("CreationDate");
+
+                    b.Property<DateTime?>("DecisionDate");
+
+                    b.Property<string>("DecisionUserId");
+
+                    b.Property<bool>("IsAccepted");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BoardId");
+
+                    b.ToTable("JoinRequests");
+                });
+
+            modelBuilder.Entity("iKudo.Domain.Model.UserBoard", b =>
+                {
+                    b.Property<string>("UserId");
+
+                    b.Property<int>("BoardId");
+
+                    b.HasKey("UserId", "BoardId");
+
+                    b.HasIndex("BoardId");
+
+                    b.ToTable("UserBoards");
+                });
+
+            modelBuilder.Entity("iKudo.Domain.Model.JoinRequest", b =>
+                {
+                    b.HasOne("iKudo.Domain.Model.Board", "Board")
+                        .WithMany("JoinRequests")
+                        .HasForeignKey("BoardId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("iKudo.Domain.Model.UserBoard", b =>
+                {
+                    b.HasOne("iKudo.Domain.Model.Board")
+                        .WithMany("UserBoards")
+                        .HasForeignKey("BoardId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
