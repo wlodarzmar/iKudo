3bdb8539061e416827ecc97bbf4dae311eb1f1ce
mwlodarz <marcin.wlodarz@pentacomp.pl>
2017-05-27 16:04:51 +0200

    init migration added

---
+++ iKudo/src/iKudo.Domain/Migrations/20170527132936_Init.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace iKudo.Domain.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    CreatorId = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ModificationDate = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JoinRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BoardId = table.Column<int>(nullable: false),
+                    CandidateId = table.Column<string>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    DecisionDate = table.Column<DateTime>(nullable: true),
+                    DecisionUserId = table.Column<string>(nullable: true),
+                    IsAccepted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JoinRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JoinRequests_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserBoards",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    BoardId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserBoards", x => new { x.UserId, x.BoardId });
+                    table.ForeignKey(
+                        name: "FK_UserBoards_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JoinRequests_BoardId",
+                table: "JoinRequests",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserBoards_BoardId",
+                table: "UserBoards",
+                column: "BoardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JoinRequests");
+
+            migrationBuilder.DropTable(
+                name: "UserBoards");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
