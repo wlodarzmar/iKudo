0b9b93df09b934da7cad37f34e664a0e0c868a17
wlodarzmar <wlodarzmar@gmail.com>
2017-06-06 19:53:42 +0200

    migrations

---
+++ iKudo/src/iKudo.Domain/Migrations/20170604071521_JoinStatusAdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace iKudo.Domain.Migrations
+{
+    public partial class JoinStatusAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "JoinRequests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "JoinRequests");
+        }
+    }
+}
