72b6567ee5e8a18eef518bf27d6323c36f8818bd
wlodarzmar <wlodarzmar@gmail.com>
2017-06-08 18:13:04 +0200

    notification model

---
+++ iKudo/src/iKudo.Domain/Enums/NotificationTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace iKudo.Domain.Enums
+{
+    public enum NotificationTypes
+    {
+        BoardJoinAccepted = 1,
+        BoardJoinRejected = 2,
+        BoardJoinAdded = 3
+    }
+}
---
+++ iKudo/src/iKudo.Domain/Model/Notification.cs
@@ -0,0 +1,26 @@
+﻿using iKudo.Domain.Enums;
+using System;
+
+namespace iKudo.Domain.Model
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        public NotificationTypes Type { get; set; }
+
+        public string SenderId { get; set; }
+
+        public string ReceiverId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int? BoardId { get; set; }
+
+        public virtual Board Board { get; set; }
+
+        public DateTime? ReadDate { get; set; }
+
+        public bool IsRead => ReadDate.HasValue;
+    }
+}
