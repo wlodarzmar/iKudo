73054d5acd1308b0ed5c984c5728c93d1560a64e
mwlodarz <marcin.wlodarz@pentacomp.pl>
2017-08-09 14:12:36 +0200

    dashboard menu

--- iKudo/src/iKudo.Clients.Web/src/app.ts
+++ iKudo/src/iKudo.Clients.Web/src/app.ts
@@ -16,13 +16,14 @@ export class App {
     public configureRouter(config: RouterConfiguration, router: Router) {
         config.title = 'iKudo';
         config.map([
-            { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' },
+
+            { route: '/', name: 'dashboard', moduleId: 'dashboard/dashboard', title: 'Pulpit' },
+            { route: 'dashboard', name: 'dashboard', moduleId: 'dashboard/dashboard', nav: true, title: 'Pulpit' },
             { route: 'boards/add', name: 'addboard', moduleId: 'board/addBoard' },
             { route: 'boards', name: 'boards', moduleId: 'board/boards', nav: true, title: 'Tablice' },
             { route: 'boards/:id/edit', name: 'editBoard', moduleId: 'board/editBoard' },
             { route: 'boards/:id/details', name: 'boardDetails', moduleId: 'board/boardDetails' },
             { route: 'boards/:id', name: 'boardPreview', moduleId: 'board/preview' },
-
             { route: 'boards/:id/kudos/add', name: 'addKudo', moduleId: 'kudo/addKudo' }
         ]);
 
