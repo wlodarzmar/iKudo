82c88d3038600dd75c02fc3db7ae34971af86af9
mwlodarz <marcin.wlodarz@pentacomp.pl>
2017-08-10 15:42:10 +0200

    clear btn, options string enum

--- iKudo/src/iKudo.Clients.Web/src/kudo/mykudo.ts
+++ iKudo/src/iKudo.Clients.Web/src/kudo/mykudo.ts
@@ -1,2 +1,25 @@
 ﻿export class MyKudo {
+
+	constructor(){
+		this.kudoTypes = [MyKudoSearchOptions.All, MyKudoSearchOptions.Received, MyKudoSearchOptions.Sended];
+		this.selectedKudoType = MyKudoSearchOptions.All;
+	}
+
+	public kudoTypes: MyKudoSearchOptions[];
+	public selectedKudoType: MyKudoSearchOptions;
+
+	public submit() {
+		console.log(this.selectedKudoType);
+	}
+
+	public refreshSearch() {
+		this.selectedKudoType = MyKudoSearchOptions.All;
+	}
+}
+
+enum MyKudoSearchOptions {
+
+	All = "Wszystkie",
+	Received = "Otrzymane",
+	Sended = "Wysłane"
 }
