548574dd418fb106f35edde02eac0e47a3f1f416
MWlodarz <wlodarzmar@gmail.com>
2018-04-28 21:11:02 +0200

    US-215 remove commented code

--- iKudo/src/iKudo.Clients.Web/ClientApp/app/services/models/notification.ts
+++ iKudo/src/iKudo.Clients.Web/ClientApp/app/services/models/notification.ts
@@ -15,30 +15,3 @@ export class Notification {
     title: string;
     message: string;
 }
-
-
- //public int Id { get; set; }
-
- //       public NotificationTypes Type { get; set; }
-
- //       public string TypeName => Type.ToString().ToLower();
-
- //       [Required]
- //       public string SenderId { get; set; }
-
- //       public UserDto Sender { get; set; }
-
- //       [Required]
- //       public string ReceiverId { get; set; }
-
- //       public UserDto Receiver { get; set; }
-
- //       public DateTime CreationDate { get; set; }
-
- //       public int? BoardId { get; set; }
-
- //       public DateTime? ReadDate { get; set; }
-
- //       public BoardDto Board { get; set; }
-
- //       public bool IsRead => ReadDate.HasValue;
