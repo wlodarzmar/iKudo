2f66816b11d53b08891c391d5b8f7ded311e9a92
MWlodarz <wlodarzmar@gmail.com>
2018-04-29 10:39:26 +0200

    US-186 example pie chart

--- iKudo/src/iKudo.Clients.Web/ClientApp/app/components/dashboard/dashboard.ts
+++ iKudo/src/iKudo.Clients.Web/ClientApp/app/components/dashboard/dashboard.ts
@@ -1,2 +1,27 @@
-﻿export class Dashboard {
+﻿import { Chart } from 'chart.js';
+
+export class Dashboard {
+
+    attached() {
+
+        let data = {
+            datasets: [{
+                data: [10, 20, 30]
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue'
+            ]
+        };
+
+        var ctx = document.getElementById("myChart") as HTMLCanvasElement;
+        var myDoughnutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: data
+            //options: options
+        });
+    }
 }
