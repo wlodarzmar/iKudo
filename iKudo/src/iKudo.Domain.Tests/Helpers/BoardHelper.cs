fe4b2749175a32812904a34febe6d95410290e9c
MWlodarz <wlodarzmar@gmail.com>
2018-08-05 12:20:56 +0200

    BUG-293 fix

---
+++ iKudo/src/iKudo.Domain.Tests/Helpers/BoardHelper.cs
@@ -0,0 +1,37 @@
+﻿using iKudo.Domain.Model;
+using System;
+
+namespace iKudo.Domain.Tests.Helpers
+{
+    class BoardHelper
+    {
+        public static Board CreateBoard()
+        {
+            return new Board
+            {
+                Id = 1,
+                Name = "name",
+                CreatorId = "boardCreatorId",
+                AcceptanceType = Enums.AcceptanceType.None,
+                CreationDate = DateTime.Now,
+                Description = "board description",
+                IsPrivate = true,
+            };
+        }
+
+        public static BoardInvitation CreateBoardInvitation(Board board)
+        {
+            return new BoardInvitation
+            {
+                Id = 1,
+                Board = board,
+                BoardId = board.Id,
+                Code = Guid.NewGuid(),
+                IsActive = true,
+                CreationDate = DateTime.Now,
+                CreatorId = board.CreatorId,
+                Email = "email@email.com"
+            };
+        }
+    }
+}
